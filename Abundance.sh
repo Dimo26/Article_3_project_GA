5516214333bfae4459c0a3591585f27002904cd5
Dima Mohsin <dimam@rackham3.uppmax.uu.se>
2025-04-28 13:42:58 +0200

    Add Abundance of organisms

---
+++ Abundance.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -l
+
+#SBATCH -A uppmax2025-3-3
+#SBATCH -M snowy 
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 08:00:00
+#SBATCH -J Abundance_Bins_Organisms
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user dima.mohsin.1749@student.uu.se
+
+#Load modules
+
+module load bioinfo-tools
+module load bwa/0.7.8
+module load samtools
+
+BINS="/home/dimam/UPPMAX_Labs/results/250422_HQ_bins"
+DNA="/home/dimam/UPPMAX_Labs"
+OUTPUT_Dir="/home/dimam/UPPMAX_Labs/results/250422_Abundance"
+for bin in 250410_Bin_DNA_5 250410_Bin_DNA_11 250410_Bin_DNA_12 250410_Bin_DNA_24 250410_Bin_DNA_25
+do 
+    bwa index $BINS/${bin}.fa
+    bwa mem $BINS/${bin}.fa $DNA/SRR4342129_1.paired.trimmed.fastq.gz $DNA/SRR4342129_2.paired.trimmed.fastq.gz | samtools view -u -b > $OUTPUT_Dir/${bin}.bam
+    samtools flagstat $OUTPUT_Dir/${bin}.bam > ${OUTPUT_Dir}/${bin}_flagstat.txt    
+done
+
+
+for bin in 250410_Bin_DNA_5 250410_Bin_DNA_11 250410_Bin_DNA_12 250410_Bin_DNA_24 250410_Bin_DNA_25
+do
+    bwa index $BINS/${bin}.fa
+    bwa mem $BINS/${bin}.fa $DNA/SRR4342133_1.paired.trimmed.fastq.gz $DNA/SRR4342133_2.paired.trimmed.fastq.gz | samtools view -u -b > $OUTPUT_Dir/${bin}.bam
+    samtools flagstat $OUTPUT_Dir/${bin}.bam > ${OUTPUT_Dir}/${bin}_flagstat.txt
+done
+
