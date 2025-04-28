a17f8d06d96f863d15df0d5f4ed547075402a578
Dima Mohsin <dimam@rackham3.uppmax.uu.se>
2025-04-28 13:43:12 +0200

    Add Expression of organisms

---
+++ Expression.sh
@@ -0,0 +1,36 @@
+#! /bin/bash -l
+
+#SBATCH -A uppmax2025-3-3
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 08:00:00
+#SBATCH -J Expression_RNA
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user dima.mohsin.1749@student.uu.se
+
+#Load modules
+
+module load bioinfo-tools
+module load bwa/0.7.8
+module load samtools
+
+
+BINS="/home/dimam/UPPMAX_Labs/results/250422_HQ_bins"
+RNA="/home/dimam/UPPMAX_Labs/Trimmed_RNA"
+OUTPUT_Dir="/home/dimam/UPPMAX_Labs/results/250425_Expression"
+for bin in 250410_Bin_DNA_5 250410_Bin_DNA_11 250410_Bin_DNA_12 250410_Bin_DNA_24 250410_Bin_DNA_25
+do 
+    bwa index $BINS/${bin}.fa
+    bwa mem $BINS/${bin}.fa $RNA/SRR4342137.1.paired.trimmed.fastq.gz $RNA/SRR4342137.2.paired.trimmed.fastq.gz | samtools view -u -b > $OUTPUT_Dir/${bin}.bam
+    samtools flagstat $OUTPUT_Dir/${bin}.bam > ${OUTPUT_Dir}/${bin}_flagstat.txt    
+done
+
+for bin in 250410_Bin_DNA_5 250410_Bin_DNA_11 250410_Bin_DNA_12 250410_Bin_DNA_24 250410_Bin_DNA_25
+do
+    bwa index $BINS/${bin}.fa
+    bwa mem $BINS/${bin}.fa $RNA/SRR4342139.1.paired.trimmed.fastq.gz $RNA/SRR4342139.2.paired.trimmed.fastq.gz | samtools view -u -b > $OUTPUT_Dir/${bin}.bam
+    samtools flagstat $OUTPUT_Dir/${bin}.bam > ${OUTPUT_Dir}/${bin}_flagstat.txt
+done
+
+
