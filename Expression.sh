4cc77ccaad3079785815d09db995fa3c1f893e41
Dima Mohsin <dimam@rackham1.uppmax.uu.se>
2025-05-02 08:32:34 +0200

    Add Expression of organisms

--- Expression.sh
+++ Expression.sh
@@ -18,19 +18,19 @@ module load samtools
 
 BINS="/home/dimam/UPPMAX_Labs/results/250422_HQ_bins"
 RNA="/home/dimam/UPPMAX_Labs/Trimmed_RNA"
-OUTPUT_Dir="/home/dimam/UPPMAX_Labs/results/250425_Expression"
+OUTPUT_Dir="/proj/uppmax2025-3-3/UPPMAX_Labs/results/250501_Expression_RNA"
 for bin in 250410_Bin_DNA_5 250410_Bin_DNA_11 250410_Bin_DNA_12 250410_Bin_DNA_24 250410_Bin_DNA_25
 do 
     bwa index $BINS/${bin}.fa
-    bwa mem $BINS/${bin}.fa $RNA/SRR4342137.1.paired.trimmed.fastq.gz $RNA/SRR4342137.2.paired.trimmed.fastq.gz | samtools view -u -b > $OUTPUT_Dir/${bin}_SRR43432137.bam
-    samtools flagstat $OUTPUT_Dir/${bin}_SRR43432137.bam > ${OUTPUT_Dir}/${bin}_SRR43432137_flagstat.txt    
+    bwa mem $BINS/${bin}.fa $RNA/SRR4342137.1.paired.trimmed.fastq.gz $RNA/SRR4342137.2.paired.trimmed.fastq.gz | samtools view -u -b > $OUTPUT_Dir/${bin}_SRR4342137.bam
+    samtools flagstat $OUTPUT_Dir/${bin}_SRR4342137.bam > ${OUTPUT_Dir}/${bin}_SRR4342137_flagstat.txt    
 done
 
 for bin in 250410_Bin_DNA_5 250410_Bin_DNA_11 250410_Bin_DNA_12 250410_Bin_DNA_24 250410_Bin_DNA_25
 do
     bwa index $BINS/${bin}.fa
-    bwa mem $BINS/${bin}.fa $RNA/SRR4342139.1.paired.trimmed.fastq.gz $RNA/SRR4342139.2.paired.trimmed.fastq.gz | samtools view -u -b > $OUTPUT_Dir/${bin}_SRR43432139.bam
-    samtools flagstat $OUTPUT_Dir/${bin}_SRR43432139.bam > ${OUTPUT_Dir}/${bin}_SRR43432139_flagstat.txt
+    bwa mem $BINS/${bin}.fa $RNA/SRR4342139.1.paired.trimmed.fastq.gz $RNA/SRR4342139.2.paired.trimmed.fastq.gz | samtools view -u -b > $OUTPUT_Dir/${bin}_SRR4342139.bam
+    samtools flagstat $OUTPUT_Dir/${bin}_SRR4342139.bam > ${OUTPUT_Dir}/${bin}_SRR4342139_flagstat.txt
 done
 
 
